52de4fd1fd52f14c38ef2f35ddf847c451abea70
Dulaj Nimal <dulajnirmal30@gmail.com>
2022-06-12 18:10:33 +0530

    Created admin side of payroll

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+
 function Home(){
     return (
     <div>
         <center>
             <h1>Welcome to Lisa!</h1>
             <h2>HRMS - SmashTaps Pvt Ltd.</h2>
+            
         </center>
-    </div>
+       
+</div>
+
     )
 }
 
