8bf6fbc374f4277323051ac5f7cd42c292b17bf5
TharushiDithya <97993879+TharushiDithya@users.noreply.github.com>
2022-06-23 05:35:12 +0530

    updated submit button

--- src/Components/RewardsComponent/RewardDisplay.js
+++ src/Components/RewardsComponent/RewardDisplay.js
@@ -1,42 +1,28 @@
 import React from "react";
 import {Input } from "antd";
 import { InputNumber, Button, Progress } from "antd";
-
-
+import "./RewardCss.css";
 
   function onChange(value) {
     console.log("changed", value);
   } 
-
 const RewardDisplay =() =>{
     return(
-  
-    <div >
-  
+    <div > 
           <div><h2 className="txt"> Your current avaliable points:</h2></div>
           <div className = "ProgressB">
       <Progress
       type="circle"
       strokeColor={{
         '0%': '#108ee9',
-        '100%': '#87d068',
-        
-      }}
+        '100%': '#87d068', }}
       width="250px"
-      percent={80}
-      
-      /></div>
-      
-      
-       <div>
-    
-       <h2 className="txt"> Offer points:</h2>
-       <InputNumber min={1} max={10} onChange={onChange} style={{marginTop:"50px", marginLeft:"100px"}} /> 
-      
-        </div>
+      percent={80} 
+      /></div> <div>
 
-      
-       <h2 className="txt" name="Shoutout"> Shoutout:</h2>
+     <h2 className="txt"> Offer points:</h2>
+       <InputNumber min={1} max={10} onChange={onChange} style={{marginTop:"50px", marginLeft:"100px"}} />  </div>
+    <h2 className="txt" name="Shoutout"> Shoutout:</h2>
 
 <Input.TextArea
               style={{
@@ -45,22 +31,9 @@ const RewardDisplay =() =>{
                 height:"100px",
               }}
             />
-      
-
-
-       <space >
-          <Button type="primary" htmlType="submit" className="btn" >
+            <space >
+          <Button type="primary" htmlType="submit" style={{marginTop:"50px", marginLeft:"60%"}}  >
             Submit
-          </Button>
-        </space>
-        
-    </div>
-
-      
-
-       
-        
-    );
-}
-
+          </Button></space> </div>
+    );}
 export default RewardDisplay;
